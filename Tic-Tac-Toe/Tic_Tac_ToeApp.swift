6ac6961efccb4b3a0b58917599d58dacce485ac9
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-11 08:01:59 -0800

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by youlserf on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
